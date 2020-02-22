1f68af2355fea6fb36f46a376decc2907fc58920
tvarano <tvarano54@gmail.com>
2020-02-22 11:38:31 -0500

    ok flask works

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+data = {}
+
 @app.route("/")
 def home():
-   return "Hello, World!"
+   return render_template('index.html')
+
+
 if __name__ == "__main__":
    app.run(debug=True)
