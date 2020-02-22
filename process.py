a59cf2654a0743cc434769c61c52b675c08c011e
Sagar Saxena <sagardsaxena@gmail.com>
2020-02-22 14:33:38 -0500

    Fix Thread Issues

--- process.py
+++ process.py
@@ -1,17 +1,31 @@
 import sqlite3 as sql
 
-conn = sql.connect("cleanup.db")
-curs = conn.cursor()
-
 get_dense = lambda p, d: None if d == 0 else p/d
 
+def init():
+	conn = sql.connect("cleanup.db")
+	curs = conn.cursor()
+	return conn, curs
+
+def close():
+	conn.commit()
+	conn.close()
+
 def get_months_data():
-	rows = curs.execute("select Month, Year, sum(Total_Items_Collected) from cleanup where Year >= 2010 and Year < 2020 group by Month, Year order by Year ASC, Month ASC;")
-	return [(r[0], r[1], r[2]) for r in rows]
+	conn, curs = init()
+	rows = curs.execute("select Month, Year, sum(Total_Items_Collected), sum(Miles), sum(Pounds) from cleanup where Year >= 2010 and Year < 2020 group by Month, Year order by Year ASC, Month ASC;")
+	conn.close()
+	return [(r[0], r[1], r[2], get_dense(r[4], r[3])) for r in rows]
 
 def get_month_data(month, year):
+	conn, curs = init()
 	rows = curs.execute('select zone, sum(Total_Items_Collected), sum(Miles), sum(Pounds), avg(Lat), avg(Long) from cleanup where Month=? and Year=? group by Zone order by Total_Items_Collected DESC', (month, year))
-	return [(r[0], r[1], get_dense(r[3], r[2]), r[4], r[5]) for r in rows]  
+	conn.close()
+	return [(r[0], r[1], get_dense(r[3], r[2]), r[4], r[5]) for r in rows] 
+
+def get_zone_data(zone, month, year):
+	return
+ 
 
 
 
