4f0cf2cbe93b5308192a945dea5981f7a80d1575
Sagar Saxena <sagardsaxena@gmail.com>
2020-02-22 15:08:48 -0500

    Zone Specific Data

--- process.py
+++ process.py
@@ -26,8 +26,14 @@ def get_month_data(month, year):
 	return res
 
 def get_zone_data(zone, month, year):
-	return
+	conn, curs = init()
+	rows = curs.execute('select Cleanup_ID, sum(Total_Items_Collected), sum(Miles), sum(Pounds), Lat, Long from cleanup where zone= ? and Month=? and Year=? group by lat, long order by Total_Items_Collected DESC', (zone, month, year))
+	res = {r[0]: {"num_items": r[1], "desnity": get_dense(r[3], r[2]), "lat": r[4], "lng": r[5]} for r in rows}
+	conn.close()
+	return res
+
+print(get_month_data(5, 2019))
+print(get_zone_data('Kings County, Brooklyn, NY, USA', 5, 2019))
+
  
-
-
 
