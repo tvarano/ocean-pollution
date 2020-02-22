d6b5b8d7dacfe5c9b9b054b22a093e810d8b417a
tvarano <tvarano54@gmail.com>
2020-02-22 13:46:24 -0500

    pass data with json

---
+++ static/js/map.js
@@ -0,0 +1,32 @@
+let map, heatmap, data;
+
+function toLatLng(lat, lng) {
+    return new google.maps.LatLng(lat, lng);
+}
+
+function fromJSON(json) {
+    let input = JSON.parse(json.replace(/&#34;/g, '"'))
+    console.log(input)
+    data = [];
+    for (let i = 0; i < input.length; i++) {
+        data.push(toLatLng(input[i].lat, input[i].lng))
+        console.log(toLatLng(input[i].lat, input[i].lng))
+    }
+}
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: {lat: 40, lng: -75.146},
+    mapTypeId: 'satellite'
+});
+
+heatmap = new google.maps.visualization.HeatmapLayer({
+        data: data,
+        map: map
+    });
+}
+  
+function toggleHeatmap() {
+    heatmap.setMap(heatmap.getMap() ? null : map);
+}
